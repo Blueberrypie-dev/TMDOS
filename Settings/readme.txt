72b4ad6e0a5b04f3d8ae089a92f2b18e1b4a607a
Blueberrypie-dev <arthur.fguarezi@gmail.com>
2026-07-16 12:53:07 -0300

    For now, we'll focus on the command prompt.

--- Settings/readme.txt
+++
@@ -1,5 +0,0 @@
-The JSON included in this folder has the user data.
-
-The format for the users is:
-
-    "username" : "password"
--- System/Core/Paths.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-AppDir = Path(__file__).resolve().parent.parent.parent
-SettingsDir = AppDir / "System" / "SetApps"
-UsersFile = AppDir / "Settings" / "users.json"
